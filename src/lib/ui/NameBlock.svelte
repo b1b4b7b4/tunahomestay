332ea039bac602dd11723e2e57506076dd724e5b
just-noname-bing <danikfedorov41@gmail.com>
2026-02-22 17:19:37 +0700

    save

--- src/lib/ui/NameBlock.svelte
+++ src/lib/ui/NameBlock.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <section
-	class="bg-[#fefcf2] capitalize py-[10px] flex flex-col items-center text-center px-2"
+	class="capitalize py-[10px] flex flex-col items-center text-center px-2"
 >
 	<h1 class="text-[52px] mb-[20px]">{name}</h1>
 	<p class="text-[25px] mb-[100px] max-w-[550px] m-auto text-center font-light">
-		{description}
+		{@html description}
 	</p>
 </section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -159,7 +159,7 @@
 				<SimpleButton href="/near">Near us</SimpleButton>
 				<SimpleButton href="/tours">Tours</SimpleButton>
 			</div>
-			<div class="max-w-[583px] m-auto text-[20px]">
+			<div class="max-w-[583px] m-auto text-[20px] font-light">
 				Our address: 12 Alley Ng. 445 Đ. Lạc Long Quân, Xuân La, Tây Hồ, Hà Nội,
 				100000, Viet
 			</div>
@@ -337,6 +337,59 @@
 >
 	<path d="M1280 200H0V0l1280 195.5v4.5z"></path>
 </svg>
+
+<section
+	class="bg-[#252422] py-[75px] md:py-[100px] px-5 text-white text-center grid justify-center"
+>
+	<NameBlock
+		name="Contacts"
+		description="To book a room and for any questions, please call us at: <br/>+84 83 293 88 88"
+	/>
+
+	<div
+		class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 md:gap-[50px] max-w-[1200px]"
+	>
+		<div class="flex flex-col items-center">
+			<img
+				src="https://static.tildacdn.ink/tild3862-3561-4136-a361-343730666531/vector.svg"
+				alt=""
+				class="mb-7.5 md:mb-[30px] w-15 h-15 md:w-[60px] md:h-[60px]"
+			/>
+			<div class="mb-5 md:mb-[20px] text-[25px]">Contact</div>
+			<div class="text-[16px] font-light text-center">
+				Our Second Number: Our Second Number:<br />+84 85 586 71 86<br /><br
+				/>Email:<br />tunahomestay01@gmail.com
+			</div>
+		</div>
+
+		<div class="flex flex-col items-center">
+			<img
+				src="https://static.tildacdn.ink/tild3862-3561-4136-a361-343730666531/vector.svg"
+				alt=""
+				class="mb-7.5 md:mb-[30px] w-15 h-15 md:w-[60px] md:h-[60px]"
+			/>
+			<div class="mb-5 md:mb-[20px] text-[25px]">Address</div>
+			<div class="text-[16px] font-light text-center">
+				12 Alley Ng. 445 Đ. Lạc Long Quân, Xuân La, Tây Hồ, Hà Nội, 100000,
+				Vietnam
+			</div>
+		</div>
+
+		<div class="flex flex-col items-center">
+			<img
+				src="https://static.tildacdn.ink/tild3862-3561-4136-a361-343730666531/vector.svg"
+				alt=""
+				class="mb-7.5 md:mb-[30px] w-15 h-15 md:w-[60px] md:h-[60px]"
+			/>
+			<div class="mb-5 md:mb-[20px] text-[25px]">How to find us</div>
+			<div class="text-[16px] font-light text-center">
+				We are located in the Western part of the city. If you are coming from
+				the airport, take the 90th bus (until the Qua Nga 3 Xuan La 70m - Lac
+				Long Quan (Ho Tay)).
+			</div>
+		</div>
+	</div>
+</section>
 
 <style>
 	.cursor {
